06262af9c7e696e09e113e1dc386a6325030e41c
kamoun said <said.kamoun@gmail.com>
2022-11-21 13:51:32 +0100

    add flex box com with lazy loading module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './shared/material.module';
-import { FormsModule } from '@angular/forms';
 
+import {Routes,RouterModule} from '@angular/router';
+const routes:Routes=[
+  {path:'demo',loadChildren:()=>import('./demo/demo.module').then(m=>m.DemoModule)},
+  {
+    path:'**',redirectTo:'demo'
+  }
 
+]
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MaterialModule,
-    FormsModule   // bcz we will some angualr material that have this as dependency
+    BrowserAnimationsModule
+       // bcz we will some angualr material that have this as dependency
+       ,
+       RouterModule.forRoot(routes)
    
   ],
   providers: [],
