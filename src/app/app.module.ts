917ec54329bfc8edf7f45dadf156305420cfbd6b
kamoun said <said.kamoun@gmail.com>
2022-11-21 12:51:05 +0100

    move material module dep to another module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MaterialModule } from './shared/material.module';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -14,9 +14,9 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatCheckboxModule
+    MaterialModule,
+    FormsModule   // bcz we will some angualr material that have this as dependency
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule
+  ]
+})
+export class MaterialModule { }
